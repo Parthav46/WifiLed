6088f9f7cf74b9ddab987a3d811278fc3e43913e
Parthav46 <parthav46@gmail.com>
2022-03-27 18:26:49 +0530

    test fast led

--- examples/WifiLed/WifiLed.ino
+++ examples/WifiLed/WifiLed.ino
@@ -1,16 +1,33 @@
-#include "WifiLed.h"
+#include <FastLED.h>
 
-WifiLed led;
+#define NUM_LEDS 15
+#define DATA_PIN D0
+#define RGB_ORDER GRB
+
+CRGB leds[NUM_LEDS];
 
 void setup()
 {
-    led = WifiLed(LED_BUILTIN);
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB_ORDER>(leds, NUM_LEDS);
+    Serial.begin(115200);
+    int8_t Brightness = 10;
+    delay(1000);
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::White;
+    }
+    FastLED.setBrightness(Brightness);
+    FastLED.show();
+    delay(1000);
+    Serial.println("Reading brightness now:");
 }
 
 void loop()
 {
-    led.on();
-    delay(1000);
-    led.off();
-    delay(1000);
+    if (Serial.available()) {
+        uint8_t Brightness = Serial.readStringUntil('\n').toInt();
+        Serial.println(Brightness);
+        FastLED.setBrightness(Brightness);
+        FastLED.show();
+    }
 }
